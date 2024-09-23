d9a3d2656895e4682227d7d29df0cdcd860ac39a
yongun2 <djkye0001@naver.com>
2024-09-23 17:17:01 +0900

    chore: project init

---
+++ src/main/java/com/beyond/easycheck/EasycheckApplication.java
@@ -0,0 +1,13 @@
+package com.beyond.easycheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasycheckApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasycheckApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/beyond/easycheck/EasycheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beyond.easycheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasycheckApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
