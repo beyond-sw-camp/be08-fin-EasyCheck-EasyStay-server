6c7d711c8e6b18e354f8f71dc5725ada5a94cf5c
Jihoon Lim <bo9701@naver.com>
2024-09-25 17:46:45 +0900

    refactor: @NotBlank를 사용해서 Null Check

--- src/main/java/com/beyond/easycheck/accomodations/ui/requestbody/AccommodationUpdateRequest.java
+++ src/main/java/com/beyond/easycheck/accomodations/ui/requestbody/AccommodationUpdateRequest.java
@@ -3,6 +3,7 @@ package com.beyond.easycheck.accomodations.ui.requestbody;
 import com.beyond.easycheck.accomodations.infrastructure.entity.AccommodationType;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -12,9 +13,12 @@ import lombok.RequiredArgsConstructor;
 @Getter
 public class AccommodationUpdateRequest {
 
+    @NotBlank
     private String name;
 
+    @NotBlank
     private String address;
 
+    @Enumerated(EnumType.STRING)
     private AccommodationType accommodationType;
 }
