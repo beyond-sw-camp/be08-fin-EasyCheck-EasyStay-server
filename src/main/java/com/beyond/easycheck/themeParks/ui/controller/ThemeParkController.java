d2f56ee8b2e45d854870a2dee15d11eb07462454
hoyeon96 <hoyen96@naver.com>
2024-09-25 16:58:29 +0900

    refactor: 코드 단순화

--- src/main/java/com/beyond/easycheck/themeParks/ui/controller/ThemeParkController.java
+++ src/main/java/com/beyond/easycheck/themeParks/ui/controller/ThemeParkController.java
@@ -4,7 +4,6 @@ import com.beyond.easycheck.themeparks.application.service.ThemeParkOperationUse
 import com.beyond.easycheck.themeparks.application.service.ThemeParkOperationUseCase.ThemeParkCreateCommand;
 import com.beyond.easycheck.themeparks.application.service.ThemeParkReadUseCase;
 import com.beyond.easycheck.themeparks.application.service.ThemeParkReadUseCase.FindThemeParkResult;
-import com.beyond.easycheck.themeparks.application.service.ThemeParkReadUseCase.ThemeParkFindQuery;
 import com.beyond.easycheck.themeparks.ui.requestbody.ThemeParkCreateRequest;
 import com.beyond.easycheck.themeparks.ui.view.ApiResponseView;
 import com.beyond.easycheck.themeparks.ui.view.ThemeParkView;
