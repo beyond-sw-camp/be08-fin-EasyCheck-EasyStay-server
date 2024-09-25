63320e9ddda7e4a89667b437ed57bef39a2791d0
hoyeon96 <hoyen96@naver.com>
2024-09-25 11:26:09 +0900

    feat: ThemeParks 엔티티 생성

---
+++ src/main/java/com/beyond/easycheck/themeParks/infrastructure/persistence/entity/ThemeParks.java
@@ -0,0 +1,17 @@
+package com.beyond.easycheck.themeParks.infrastructure.persistence.entity;
+
+
+public class ThemeParks {
+
+    private int id;
+
+    private String name;
+
+    private String description;
+
+    private String location;
+
+    private String image;
+
+    private int accomodationId;
+}
