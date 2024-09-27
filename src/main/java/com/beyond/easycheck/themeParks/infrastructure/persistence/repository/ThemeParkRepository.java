b43784be3711cbc51dd5c33bdc661957d7b7c1af
hoyeon96 <hoyen96@naver.com>
2024-09-27 09:04:29 +0900

    feat:  ThemeParkRepository에 중복검색을 위한 existsByNameAndLocation 추가

--- src/main/java/com/beyond/easycheck/themeParks/infrastructure/persistence/repository/ThemeParkRepository.java
+++ src/main/java/com/beyond/easycheck/themeParks/infrastructure/persistence/repository/ThemeParkRepository.java
@@ -3,11 +3,7 @@ package com.beyond.easycheck.themeparks.infrastructure.persistence.repository;
 import com.beyond.easycheck.themeparks.infrastructure.persistence.entity.ThemeParkEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface ThemeParkRepository extends JpaRepository<ThemeParkEntity, Long> {
-
-    List<ThemeParkEntity> findAll();
 
     boolean existsByNameAndLocation(String name, String location);
 }
