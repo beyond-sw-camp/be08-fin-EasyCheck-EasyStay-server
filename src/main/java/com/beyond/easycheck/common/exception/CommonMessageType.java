e02956580697d1af0a93ed94b58c6ee8b5bef45a
hoyeon96 <hoyen96@naver.com>
2024-09-27 15:21:32 +0900

    refactor: common exception case 추가 (DATABASE_CONNECTION_FAILED, UNKNOWN_ERROR)

--- src/main/java/com/beyond/easycheck/common/exception/CommonMessageType.java
+++ src/main/java/com/beyond/easycheck/common/exception/CommonMessageType.java
@@ -17,6 +17,8 @@ public enum CommonMessageType implements MessageType {
 
     SQL_EXCEPTION_ERROR("", HttpStatus.INTERNAL_SERVER_ERROR),
     INTERNAL_SERVER_ERROR("An error occurred inside the server.", HttpStatus.INTERNAL_SERVER_ERROR),
+    DATABASE_CONNECTION_FAILED("데이터베이스 연결에 실패했습니다.", HttpStatus.INTERNAL_SERVER_ERROR),
+    UNKNOWN_ERROR("알 수 없는 오류가 발생했습니다. 관리자에게 문의하세요.", HttpStatus.INTERNAL_SERVER_ERROR)
     ;
     private final String message;
     private final HttpStatus status;
