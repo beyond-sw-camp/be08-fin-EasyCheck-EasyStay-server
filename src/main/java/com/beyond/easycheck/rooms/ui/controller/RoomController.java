8a58fb4a1685b9dbd3091402cc7953034ec5acb4
DESKTOP-D6PGV2R\Playdata <knah0713@hanmail.net>
2024-09-27 17:43:36 +0900

    refactor: import 수정

--- src/main/java/com/beyond/easycheck/rooms/ui/controller/RoomController.java
+++ src/main/java/com/beyond/easycheck/rooms/ui/controller/RoomController.java
@@ -3,13 +3,13 @@ package com.beyond.easycheck.rooms.ui.controller;
 import com.beyond.easycheck.rooms.application.service.RoomService;
 import com.beyond.easycheck.rooms.ui.requestbody.RoomCreateRequest;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
