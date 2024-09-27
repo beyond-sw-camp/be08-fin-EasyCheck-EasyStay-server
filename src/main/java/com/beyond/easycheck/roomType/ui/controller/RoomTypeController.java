b36fd35c7e46a2300b86562de80d733dff69d216
DESKTOP-D6PGV2R\Playdata <knah0713@hanmail.net>
2024-09-27 11:16:41 +0900

    feat: 객실 유형 조회 기능 추가

--- src/main/java/com/beyond/easycheck/roomType/ui/controller/RoomTypeController.java
+++ src/main/java/com/beyond/easycheck/roomType/ui/controller/RoomTypeController.java
@@ -2,12 +2,15 @@ package com.beyond.easycheck.roomType.ui.controller;
 
 import com.beyond.easycheck.roomType.application.service.RoomTypeService;
 import com.beyond.easycheck.roomType.ui.requestbody.RoomTypeCreateRequest;
+import com.beyond.easycheck.roomType.ui.requestbody.RoomTypeReadRequest;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -22,6 +25,20 @@ public class RoomTypeController {
     public ResponseEntity<Void> createRoomType(@RequestBody RoomTypeCreateRequest roomTypeCreateRequest) {
         roomTypeService.createRoomType(roomTypeCreateRequest);
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @GetMapping("/{id}")
+    @Operation(summary = "객실 유형 조회 API")
+    public ResponseEntity<RoomTypeReadRequest> readRoomType(@PathVariable Long id) {
+        RoomTypeReadRequest roomTypeReadRequest = roomTypeService.readRoomType(id);
+        return ResponseEntity.ok().body(roomTypeReadRequest);
+    }
+
+    @GetMapping("")
+    @Operation(summary = "객실 유형 전체 조회 API")
+    public ResponseEntity<List<RoomTypeReadRequest>> readRoomTypes() {
+        List<RoomTypeReadRequest> roomTypeReadRequests = roomTypeService.readRoomTypes();
+        return ResponseEntity.ok().body(roomTypeReadRequests);
     }
 
     @DeleteMapping("/{id}")
---
+++ src/main/java/com/beyond/easycheck/roomType/ui/requestbody/RoomTypeReadRequest.java
@@ -0,0 +1,29 @@
+package com.beyond.easycheck.roomType.ui.requestbody;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+@Builder
+public class RoomTypeReadRequest {
+
+    private Long roomTypeId;
+
+    private Long accomodationId;
+
+    private String typeName;
+
+    private String description;
+
+    private int maxOccupancy;
+
+    public RoomTypeReadRequest(Long roomTypeId, Long accomodationId, String typeName, String description, int maxOccupancy) {
+        this.roomTypeId = roomTypeId;
+        this.accomodationId = accomodationId;
+        this.typeName = typeName;
+        this.description = description;
+        this.maxOccupancy = maxOccupancy;
+    }
+}
