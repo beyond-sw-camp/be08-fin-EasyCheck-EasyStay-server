51a36856d6f19cf559ec00333593318825fcde3b
Jihoon Lim <bo9701@naver.com>
2024-09-29 23:55:04 +0900

    feat: 객실 예약 취소 기능 추가

--- src/main/java/com/beyond/easycheck/reservationroom/infrastructure/entity/ReservationRoomEntity.java
+++ src/main/java/com/beyond/easycheck/reservationroom/infrastructure/entity/ReservationRoomEntity.java
@@ -1,6 +1,7 @@
 package com.beyond.easycheck.reservationroom.infrastructure.entity;
 
 import com.beyond.easycheck.common.entity.BaseTimeEntity;
+import com.beyond.easycheck.reservationroom.ui.requestbody.ReservationRoomUpdateRequest;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -9,6 +10,7 @@ import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -41,4 +43,12 @@ public class ReservationRoomEntity extends BaseTimeEntity {
     @Column(nullable = false)
     @Enumerated(EnumType.STRING)
     private PaymentStatus paymentStatus;
+
+    public void updateReservationRoom(ReservationRoomUpdateRequest reservationRoomUpdateRequest) {
+        Optional.ofNullable(reservationRoomUpdateRequest.getCheckinDate()).ifPresent(checkinDate -> this.checkinDate = checkinDate);
+        Optional.ofNullable(reservationRoomUpdateRequest.getCheckoutDate()).ifPresent(checkoutDate -> this.checkoutDate = checkoutDate);
+        Optional.ofNullable(reservationRoomUpdateRequest.getReservationStatus()).ifPresent(reservationStatus -> this.reservationStatus = reservationStatus);
+        Optional.ofNullable(reservationRoomUpdateRequest.getTotalPrice()).ifPresent(totalPrice -> this.totalPrice = totalPrice);
+        Optional.ofNullable(reservationRoomUpdateRequest.getPaymentStatus()).ifPresent(paymentStatus -> this.paymentStatus = paymentStatus);
+    }
 }
