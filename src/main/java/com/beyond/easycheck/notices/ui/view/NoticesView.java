0c8e819e9bd93e01d96471105a8441d11ce71d9e
jeongjinjoo <enjoy2573@gmail.com>
2024-09-30 11:05:47 +0900

    refactor : accommodation와 매핑

--- src/main/java/com/beyond/easycheck/notices/ui/view/NoticesView.java
+++ src/main/java/com/beyond/easycheck/notices/ui/view/NoticesView.java
@@ -1,9 +1,13 @@
 package com.beyond.easycheck.notices.ui.view;
 
+import com.beyond.easycheck.additionalservices.infrastructure.entity.AdditionalServiceEntity;
+import com.beyond.easycheck.additionalservices.ui.view.AdditionalServiceView;
 import com.beyond.easycheck.notices.infrastructure.persistence.entity.NoticesEntity;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -12,17 +16,25 @@ public class NoticesView {
 
     private Long id;
 
+    private String accommodationName;
+
     private String title;
 
     private String content;
 
-//    public static List<NoticesView> listOf(List<NoticesEntity>) filtered
+    public static List<NoticesView> listof(List<NoticesEntity> filteredNotices) {
 
+        return filteredNotices.stream()
+                .map(NoticesView::of)
+                .toList();
+    }
 
-    public static NoticesView of(NoticesEntity entity) {
+    public static NoticesView of(NoticesEntity noticesEntity) {
+
         return new NoticesView(
-                entity.getId(),
-                entity.getTitle(),
-                entity.getContent());
+                noticesEntity.getId(),
+                noticesEntity.getAccommodationEntity().getName(),
+                noticesEntity.getTitle(),
+                noticesEntity.getContent());
     }
 }
