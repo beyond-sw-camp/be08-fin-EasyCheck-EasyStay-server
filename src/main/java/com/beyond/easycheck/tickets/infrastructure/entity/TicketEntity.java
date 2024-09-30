47dae8ec65b867b87f34c39b5f96bb2301577668
hoyeon96 <hoyen96@naver.com>
2024-09-30 11:02:09 +0900

    feat: TicketEntity 생성

---
+++ src/main/java/com/beyond/easycheck/tickets/infrastructure/entity/TicketEntity.java
@@ -0,0 +1,78 @@
+package com.beyond.easycheck.tickets.infrastructure.entity;
+
+import com.beyond.easycheck.themeparks.infrastructure.entity.ThemeParkEntity;
+import com.beyond.easycheck.tickets.application.service.TicketOperationUseCase.TicketCreateCommand;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name = "ticket")
+public class TicketEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "theme_park_id", nullable = false)
+    private ThemeParkEntity themePark;
+
+    @Column(nullable = false)
+    private String ticketName;
+
+    @Column(nullable = false)
+    private BigDecimal price;
+
+    @Column(nullable = false)
+    private LocalDateTime saleStartDate;
+
+    @Column(nullable = false)
+    private LocalDateTime saleEndDate;
+
+    @Column(nullable = false)
+    private LocalDateTime validFromDate;
+
+    @Column(nullable = false)
+    private LocalDateTime validToDate;
+
+    public static TicketEntity createTicket(TicketCreateCommand command, ThemeParkEntity themePark) {
+        return new TicketEntity(
+                null,
+                themePark,
+                command.getTicketName(),
+                command.getPrice(),
+                command.getSaleStartDate(),
+                command.getSaleEndDate(),
+                command.getValidFromDate(),
+                command.getValidToDate()
+        );
+    }
+
+    private TicketEntity(Long id, ThemeParkEntity themePark, String ticketName, BigDecimal price,
+                         LocalDateTime saleStartDate, LocalDateTime saleEndDate,
+                         LocalDateTime validFromDate, LocalDateTime validToDate) {
+        this.id = id;
+        this.themePark = themePark;
+        this.ticketName = ticketName;
+        this.price = price;
+        this.saleStartDate = saleStartDate;
+        this.saleEndDate = saleEndDate;
+        this.validFromDate = validFromDate;
+        this.validToDate = validToDate;
+    }
+
+    public void update(String ticketName, BigDecimal price, LocalDateTime saleStartDate,
+                       LocalDateTime saleEndDate, LocalDateTime validFromDate, LocalDateTime validToDate) {
+        this.ticketName = ticketName;
+        this.price = price;
+        this.saleStartDate = saleStartDate;
+        this.saleEndDate = saleEndDate;
+        this.validFromDate = validFromDate;
+        this.validToDate = validToDate;
+    }
+}
