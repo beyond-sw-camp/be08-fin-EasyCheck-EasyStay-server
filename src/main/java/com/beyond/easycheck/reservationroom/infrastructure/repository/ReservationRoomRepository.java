22e0a5e44b9607eaeb1e950187faf37053252bb8
Jihoon Lim <bo9701@naver.com>
2024-10-03 17:19:15 +0900

    feat: 예약 생성 시 검증 로직 작성

--- src/main/java/com/beyond/easycheck/reservationroom/infrastructure/repository/ReservationRoomRepository.java
+++ src/main/java/com/beyond/easycheck/reservationroom/infrastructure/repository/ReservationRoomRepository.java
@@ -1,9 +1,15 @@
 package com.beyond.easycheck.reservationroom.infrastructure.repository;
 
 import com.beyond.easycheck.reservationroom.infrastructure.entity.ReservationRoomEntity;
+import com.beyond.easycheck.rooms.infrastructure.entity.RoomEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+
 @Repository
 public interface ReservationRoomRepository extends JpaRepository<ReservationRoomEntity, Long> {
+
+    boolean existsByRoomEntityAndCheckinDateLessThanEqualAndCheckoutDateGreaterThanEqual(
+            RoomEntity roomEntity, LocalDate checkoutDate, LocalDate checkinDate);
 }
