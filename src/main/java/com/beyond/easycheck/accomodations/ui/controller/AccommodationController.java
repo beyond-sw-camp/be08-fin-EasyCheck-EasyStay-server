57226d8cda38cd89082c4e9ff1076622ce6acdfc
Jihoon Lim <bo9701@naver.com>
2024-10-04 10:21:27 +0900

    refactor: 불필요한 import 제거

--- src/main/java/com/beyond/easycheck/accomodations/ui/controller/AccommodationController.java
+++ src/main/java/com/beyond/easycheck/accomodations/ui/controller/AccommodationController.java
@@ -11,7 +11,6 @@ import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
--- src/main/java/com/beyond/easycheck/accomodations/ui/requestbody/AccommodationCreateRequest.java
+++ src/main/java/com/beyond/easycheck/accomodations/ui/requestbody/AccommodationCreateRequest.java
@@ -4,7 +4,6 @@ import com.beyond.easycheck.accomodations.infrastructure.entity.AccommodationTyp
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
