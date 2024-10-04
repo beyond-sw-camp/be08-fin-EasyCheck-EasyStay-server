3d041d8f3bf3add64fef9ef47a135207776d49c9
Jihoon Lim <bo9701@naver.com>
2024-10-04 14:27:23 +0900

    refactor: 결제 시 paymentstatus 값 변환

--- src/main/java/com/beyond/easycheck/mail/application/service/MailService.java
+++ src/main/java/com/beyond/easycheck/mail/application/service/MailService.java
@@ -1,8 +1,12 @@
 package com.beyond.easycheck.mail.application.service;
 
+import com.beyond.easycheck.reservationroom.ui.view.ReservationRoomView;
+
 public interface MailService {
 
     void sendVerificationCode(String email);
 
     void verifyEmail(String code);
+
+    void sendReservationConfirmationEmail(String email, ReservationRoomView reservationDetails);
 }
