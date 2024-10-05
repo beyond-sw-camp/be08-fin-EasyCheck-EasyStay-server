5f8e27c2ce3321142a78ba01f52b5175fab2e827
Jihoon Lim <bo9701@naver.com>
2024-10-05 20:18:28 +0900

    refactor: 불필요한 import 제거

--- src/main/java/com/beyond/easycheck/reservationroom/ui/requestbody/ReservationRoomCreateRequest.java
+++ src/main/java/com/beyond/easycheck/reservationroom/ui/requestbody/ReservationRoomCreateRequest.java
@@ -5,14 +5,12 @@ import com.beyond.easycheck.reservationroom.infrastructure.entity.ReservationSta
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @RequiredArgsConstructor(access = lombok.AccessLevel.PUBLIC)
--- src/main/java/com/beyond/easycheck/reservationroom/ui/view/ReservationRoomView.java
+++ src/main/java/com/beyond/easycheck/reservationroom/ui/view/ReservationRoomView.java
@@ -8,7 +8,6 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @AllArgsConstructor
