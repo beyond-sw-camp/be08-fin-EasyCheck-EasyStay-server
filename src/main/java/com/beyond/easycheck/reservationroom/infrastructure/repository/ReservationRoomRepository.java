cab072a8f6761807c8183d0ca98e519d324fd692
Jihoon Lim <bo9701@naver.com>
2024-10-05 22:17:26 +0900

    refactor: Repository에 함수 제거

--- src/main/java/com/beyond/easycheck/reservationroom/infrastructure/repository/ReservationRoomRepository.java
+++ src/main/java/com/beyond/easycheck/reservationroom/infrastructure/repository/ReservationRoomRepository.java
@@ -1,13 +1,10 @@
 package com.beyond.easycheck.reservationroom.infrastructure.repository;
 
 import com.beyond.easycheck.reservationroom.infrastructure.entity.ReservationRoomEntity;
-import com.beyond.easycheck.reservationroom.infrastructure.entity.ReservationStatus;
-import com.beyond.easycheck.rooms.infrastructure.entity.RoomEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ReservationRoomRepository extends JpaRepository<ReservationRoomEntity, Long> {
 
-    int countByRoomEntityAndReservationStatus(RoomEntity roomEntity, ReservationStatus reservationStatus);
 }
