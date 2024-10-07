a00bed041f28ab37798922b7f4dcce1ed6980458
hoyeon96 <hoyen96@naver.com>
2024-10-07 10:01:09 +0900

    feat: TicketOrderMessageType 중복취소 방지 추가

--- src/main/java/com/beyond/easycheck/tickets/exception/TicketOrderMessageType.java
+++ src/main/java/com/beyond/easycheck/tickets/exception/TicketOrderMessageType.java
@@ -10,7 +10,7 @@ import org.springframework.http.HttpStatus;
 public enum TicketOrderMessageType implements MessageType {
 
     ORDER_NOT_FOUND("해당 주문을 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
-
+    ORDER_ALREADY_CANCELLED("이미 취소된 주문입니다.", HttpStatus.BAD_REQUEST),
     ;
    private final String message;
    private final HttpStatus status;
