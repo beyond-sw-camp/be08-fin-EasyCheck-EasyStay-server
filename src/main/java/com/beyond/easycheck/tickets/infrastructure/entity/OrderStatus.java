a42fd2ec078ed63f511869b21397842951c4b406
hoyeon96 <hoyen96@naver.com>
2024-10-07 10:22:27 +0900

    refactor: OrderStatus ENUM 영어로 변경

--- src/main/java/com/beyond/easycheck/tickets/infrastructure/entity/OrderStatus.java
+++ src/main/java/com/beyond/easycheck/tickets/infrastructure/entity/OrderStatus.java
@@ -1,10 +1,8 @@
 package com.beyond.easycheck.tickets.infrastructure.entity;
 
 public enum OrderStatus {
-    결제대기,
-    주문취소,
-    결제완료,
-    결제취소,
-    결제실패,
-    환불완료
+    PENDING,    // 결제 대기
+    CANCELLED,  // 주문 취소
+    CONFIRMED,  // 결제 완료
+    COMPLETED   // 주문 완료
 }
