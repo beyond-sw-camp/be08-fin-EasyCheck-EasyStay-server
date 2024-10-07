6e95a7f024b919c0039f9362afaebc6d4fdbe908
hoyeon96 <hoyen96@naver.com>
2024-10-07 11:44:46 +0900

    feat: OrderStatus 결제 실패 ENUM 추가

--- src/main/java/com/beyond/easycheck/tickets/infrastructure/entity/OrderStatus.java
+++ src/main/java/com/beyond/easycheck/tickets/infrastructure/entity/OrderStatus.java
@@ -4,5 +4,6 @@ public enum OrderStatus {
     PENDING,    // 결제 대기
     CANCELLED,  // 주문 취소
     CONFIRMED,  // 결제 완료
-    COMPLETED   // 주문 완료
+    COMPLETED,   // 주문 완료
+    FAILED      // 결제 실패
 }
