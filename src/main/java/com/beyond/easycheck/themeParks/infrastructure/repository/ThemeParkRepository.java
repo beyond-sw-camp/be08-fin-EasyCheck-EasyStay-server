acab456fe38dce8b881a04b702e413969d9054a4
hoyeon96 <hoyen96@naver.com>
2024-10-11 16:01:06 +0900

    feat: findByAccommodationId 메소드 추가

--- src/main/java/com/beyond/easycheck/themeParks/infrastructure/repository/ThemeParkRepository.java
+++ src/main/java/com/beyond/easycheck/themeParks/infrastructure/repository/ThemeParkRepository.java
@@ -3,7 +3,11 @@ package com.beyond.easycheck.themeparks.infrastructure.repository;
 import com.beyond.easycheck.themeparks.infrastructure.entity.ThemeParkEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ThemeParkRepository extends JpaRepository<ThemeParkEntity, Long> {
 
     boolean existsByNameAndLocation(String name, String location);
+
+    List<ThemeParkEntity> findByAccommodationId(Long accommodationId);
 }
