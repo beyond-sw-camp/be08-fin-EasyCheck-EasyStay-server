53f5f61a59b67bfd98398dc5703cdc952486979e
jeongjinjoo <enjoy2573@gmail.com>
2024-10-13 02:37:40 +0900

    test : RoomtypeControllerTest 테스트 코드 작성

--- src/test/java/com/beyond/easycheck/roomtypes/application/service/RoomtypeServiceTest.java
+++ src/test/java/com/beyond/easycheck/roomtypes/application/service/RoomtypeServiceTest.java
@@ -8,11 +8,8 @@ import com.beyond.easycheck.roomtypes.exception.RoomtypeMessageType;
 import com.beyond.easycheck.roomtypes.ui.requestbody.RoomtypeCreateRequest;
 import com.beyond.easycheck.roomtypes.ui.view.RoomtypeView;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
@@ -20,7 +17,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @ActiveProfiles(profiles = {"test"})
