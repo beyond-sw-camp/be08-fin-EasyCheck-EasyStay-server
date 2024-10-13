ffb4935de66db56023fa0166a0715c26d786f656
hoyeon96 <hoyen96@naver.com>
2024-10-13 18:36:04 +0900

    feat: ThemePark s3service 적용해 이미지 업로드 오류 수정

--- src/main/java/com/beyond/easycheck/themeParks/infrastructure/repository/ThemeParkImageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.beyond.easycheck.themeparks.infrastructure.repository;
-
-import com.beyond.easycheck.themeparks.infrastructure.entity.ThemeParkEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface ThemeParkImageRepository extends JpaRepository<ThemeParkEntity, Long> {
-
-}
