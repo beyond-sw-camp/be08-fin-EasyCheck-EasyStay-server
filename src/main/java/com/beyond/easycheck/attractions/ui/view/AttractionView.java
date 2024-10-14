819399863877b9d75ba04c44c6f813abc4c3cb8d
hoyeon96 <hoyen96@naver.com>
2024-10-14 15:25:28 +0900

    refactor: image entity 생성 및 AttractionEntity에 맞게끔 View 수정

--- src/main/java/com/beyond/easycheck/attractions/ui/view/AttractionView.java
+++ src/main/java/com/beyond/easycheck/attractions/ui/view/AttractionView.java
@@ -4,18 +4,22 @@ import com.beyond.easycheck.attractions.application.service.AttractionReadUseCas
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 
+import java.util.List;
+
 @Getter
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class AttractionView {
     private final Long id;
     private final String name;
     private final String description;
-    private final String image;
+    private final Long themeParkId;
+    private final List<String> imageUrls;
 
     public AttractionView(FindAttractionResult result) {
         this.id = result.getId();
         this.name = result.getName();
         this.description = result.getDescription();
-        this.image = result.getImage();
+        this.themeParkId = result.getThemeParkId();
+        this.imageUrls = result.getImageUrls();
     }
 }
