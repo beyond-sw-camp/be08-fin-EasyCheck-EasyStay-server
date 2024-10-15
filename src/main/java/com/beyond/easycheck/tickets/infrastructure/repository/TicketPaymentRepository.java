f5389d11d23a879ffa4d16a5fa67ebf101b10bfd
hoyeon96 <hoyen96@naver.com>
2024-10-15 10:01:11 +0900

    feat: TicketPaymentEntity 에서 UserID값 통해 결제금액 내역을 보여주기 위한 쿼리 추가

--- src/main/java/com/beyond/easycheck/tickets/infrastructure/repository/TicketPaymentRepository.java
+++ src/main/java/com/beyond/easycheck/tickets/infrastructure/repository/TicketPaymentRepository.java
@@ -4,8 +4,11 @@ package com.beyond.easycheck.tickets.infrastructure.repository;
 import com.beyond.easycheck.tickets.infrastructure.entity.TicketPaymentEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface TicketPaymentRepository extends JpaRepository<TicketPaymentEntity, Long> {
     Optional<TicketPaymentEntity> findByTicketOrderId(Long ticketOrderId);
+
+    List<TicketPaymentEntity> findAllByTicketOrder_UserEntity_Id(Long userId);
 }
