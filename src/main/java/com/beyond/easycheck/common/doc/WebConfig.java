eb98de849f99121557374684d4ba041e5ff0d47b
hoyeon96 <hoyen96@naver.com>
2024-10-16 12:47:17 +0900

    feat: Front 연동을 위한 addCorsMapping 추가

--- src/main/java/com/beyond/easycheck/common/doc/WebConfig.java
+++ src/main/java/com/beyond/easycheck/common/doc/WebConfig.java
@@ -4,6 +4,7 @@ import com.beyond.easycheck.common.entity.OctetStreamReadMsgConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -16,6 +17,13 @@ public class WebConfig implements WebMvcConfigurer {
     @Autowired
     public WebConfig(OctetStreamReadMsgConverter octetStreamReadMsgConverter) {
         this.octetStreamReadMsgConverter = octetStreamReadMsgConverter;
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000") // 프론트엔드 도메인 허용
+                .allowedMethods("GET", "POST", "PATCH", "DELETE", "OPTIONS"); // 허용할 HTTP 메서드
     }
 
     @Override
