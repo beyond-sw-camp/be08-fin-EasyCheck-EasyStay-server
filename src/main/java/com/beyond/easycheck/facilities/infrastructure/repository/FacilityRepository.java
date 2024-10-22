197b00e87da2686d36bcda661b6b78aa9b0d661c
yongun2 <djkye0001@naver.com>
2024-10-22 20:07:20 +0900

    feat: 해당 업체 관리자와 관련된 부대시설 전체 조회 기능

--- src/main/java/com/beyond/easycheck/facilities/infrastructure/repository/FacilityRepository.java
+++ src/main/java/com/beyond/easycheck/facilities/infrastructure/repository/FacilityRepository.java
@@ -1,10 +1,14 @@
 package com.beyond.easycheck.facilities.infrastructure.repository;
 
 import com.beyond.easycheck.facilities.infrastructure.entity.FacilityEntity;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface FacilityRepository extends JpaRepository<FacilityEntity, Long> {
-
+    @EntityGraph(attributePaths = {"accommodationEntity"})
+    List<FacilityEntity> findAllByAccommodationEntity_Id(Long accommodationId);
 }
