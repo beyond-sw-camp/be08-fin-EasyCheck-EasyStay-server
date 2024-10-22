4ea8b5da0760a33a149e29f23a8b82f00aff061b
yongun2 <djkye0001@naver.com>
2024-10-22 20:07:33 +0900

    feat: 해당 업체 관리자와 관련된 건의사항 전체 조회 기능

--- src/main/java/com/beyond/easycheck/suggestion/infrastructure/persistence/repository/SuggestionsRepository.java
+++ src/main/java/com/beyond/easycheck/suggestion/infrastructure/persistence/repository/SuggestionsRepository.java
@@ -1,9 +1,16 @@
 package com.beyond.easycheck.suggestion.infrastructure.persistence.repository;
 
 import com.beyond.easycheck.suggestion.infrastructure.persistence.entity.SuggestionEntity;
+import jakarta.persistence.Entity;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 
 public interface SuggestionsRepository extends JpaRepository<SuggestionEntity, Long> {
+
+    @EntityGraph(attributePaths = {"userEntity"})
+    List<SuggestionEntity> findAllByAccommodationEntity_Id(Long accommodationId);
 
 }
