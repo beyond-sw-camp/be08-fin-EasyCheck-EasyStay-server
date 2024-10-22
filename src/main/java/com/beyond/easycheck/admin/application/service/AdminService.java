60794f21292f34d2dccf1c7be79a0b284ab22745
yongun2 <djkye0001@naver.com>
2024-10-22 20:07:58 +0900

    feat: 해당 업체 관리자와 관련된 전체 조회 기능

--- src/main/java/com/beyond/easycheck/admin/application/service/AdminService.java
+++ src/main/java/com/beyond/easycheck/admin/application/service/AdminService.java
@@ -1,25 +1,50 @@
 package com.beyond.easycheck.admin.application.service;
 
+import com.beyond.easycheck.additionalservices.infrastructure.repository.AdditionalServiceRepository;
+import com.beyond.easycheck.admin.exception.AdminMessageType;
 import com.beyond.easycheck.common.exception.EasyCheckException;
-import com.beyond.easycheck.user.application.service.UserReadUseCase;
+import com.beyond.easycheck.events.infrastructure.repository.EventRepository;
+import com.beyond.easycheck.facilities.infrastructure.repository.FacilityRepository;
+import com.beyond.easycheck.notices.infrastructure.persistence.repository.NoticesRepository;
+import com.beyond.easycheck.suggestion.infrastructure.persistence.repository.SuggestionsRepository;
+import com.beyond.easycheck.themeParks.infrastructure.repository.ThemeParkRepository;
 import com.beyond.easycheck.user.exception.UserMessageType;
 import com.beyond.easycheck.user.infrastructure.persistence.mariadb.entity.user.UserEntity;
 import com.beyond.easycheck.user.infrastructure.persistence.mariadb.repository.UserJpaRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static com.beyond.easycheck.user.application.service.UserReadUseCase.*;
 import static com.beyond.easycheck.user.application.service.UserReadUseCase.FindUserResult;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class AdminService implements AdminOperationUseCase, AdminReadUseCase {
 
+    private final EventRepository eventRepository;
+
     private final UserJpaRepository userJpaRepository;
+
+    private final NoticesRepository noticesRepository;
+
+    private final FacilityRepository facilityRepository;
+
+    private final ThemeParkRepository themeParkRepository;
+
+    private final SuggestionsRepository suggestionsRepository;
+
+    private final AdditionalServiceRepository additionalServiceRepository;
 
     @Override
     @Transactional
@@ -34,12 +59,86 @@ public class AdminService implements AdminOperationUseCase, AdminReadUseCase {
     }
 
     @Override
-    public List<FindUserResult> getAllUsers(UserReadUseCase.UserFindQuery query) {
+    public List<FindUserResult> getAllUsers(UserFindQuery query) {
         return List.of();
     }
 
     @Override
-    public FindUserResult getUserDetails(UserReadUseCase.UserFindQuery query) {
+    public FindUserResult getUserDetails(UserFindQuery query) {
         return null;
+    }
+
+    @Override
+    public FindAdminResult getAdminDetails() {
+        return null;
+    }
+
+    @Override
+    public List<FindSuggestionResult> getAllSuggestions() {
+        log.info("[AdminService - getAllSuggestions]");
+        return suggestionsRepository.findAllByAccommodationEntity_Id(getManagerAccommodationId())
+                .stream()
+                .map(FindSuggestionResult::findBySuggestionEntity)
+                .toList();
+    }
+
+    @Override
+    public List<FindFacilitiesResult> getAllFacilities() {
+        return facilityRepository.findAllByAccommodationEntity_Id(getManagerAccommodationId())
+                .stream()
+                .map(FindFacilitiesResult::findByFacilityEntity)
+                .toList();
+    }
+
+    @Override
+    public List<FindAdditionalServiceResult> getAllAdditionalServices() {
+        return additionalServiceRepository.findAllByAccommodationEntity_Id(getManagerAccommodationId())
+                .stream()
+                .map(FindAdditionalServiceResult::findByAdditionalServiceEntity)
+                .toList();
+    }
+
+    @Override
+    public List<FindNoticeResult> getAllNotices() {
+        return noticesRepository.findAllByAccommodationEntity_Id(getManagerAccommodationId())
+                .stream()
+                .map(FindNoticeResult::findByNoticeWithUserAndAccommodation)
+                .toList();
+    }
+
+    @Override
+    public List<FindThemeParkResult> getAllThemeParks() {
+        return themeParkRepository.findAllByAccommodation_Id(getManagerAccommodationId())
+                .stream()
+                .map(FindThemeParkResult::findByThemeParkEntity)
+                .toList();
+    }
+
+    @Override
+    public List<FindEventResult> getAllEvents() {
+        return eventRepository.findAllByAccommodationEntity_Id(getManagerAccommodationId())
+                .stream()
+                .map(FindEventResult::findByEvent)
+                .toList();
+    }
+
+    public Long getManagerAccommodationId() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        for (GrantedAuthority authority : authentication.getAuthorities()) {
+            String role = authority.getAuthority();
+
+            // ROLE_숫자_ADMIN 형식인지 확인
+            if (role.matches("ROLE_\\d+_ADMIN")) {
+                // ROLE_ 를 제거하고 _ADMIN 을 제거한 후 남은 숫자를 추출
+                String idStr = role.replace("ROLE_", "")
+                        .replace("_ADMIN", "");
+                return Long.parseLong(idStr);
+            }
+        }
+
+        // 해당하는 권한이 없을 경우 예외
+        throw new EasyCheckException(AdminMessageType.ACCOMMODATION_ADMIN_AUTHORITY_NOT_FOUND);
+
     }
 }
--- src/main/java/com/beyond/easycheck/admin/ui/controller/AdminController.java
+++ src/main/java/com/beyond/easycheck/admin/ui/controller/AdminController.java
@@ -3,9 +3,12 @@ package com.beyond.easycheck.admin.ui.controller;
 import com.beyond.easycheck.admin.application.service.AdminOperationUseCase;
 import com.beyond.easycheck.admin.application.service.AdminReadUseCase;
 import com.beyond.easycheck.admin.ui.requestbody.UserStatusUpdateRequest;
+import com.beyond.easycheck.admin.ui.view.*;
+
 import com.beyond.easycheck.user.application.service.UserReadUseCase;
 import com.beyond.easycheck.user.ui.view.UserView;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -13,10 +16,15 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+import static com.beyond.easycheck.admin.application.service.AdminReadUseCase.*;
+
 @Slf4j
 @RestController
-@RequestMapping("/api/v1/admin")
 @RequiredArgsConstructor
+@Tag(name = "관리자 관련 API")
+@RequestMapping("/api/v1/admin")
 public class AdminController {
 
     private final AdminOperationUseCase adminOperationUseCase;
@@ -32,5 +40,75 @@ public class AdminController {
 
         return ResponseEntity.status(HttpStatus.OK)
                 .body(new UserView(result));
+    }
+
+    @GetMapping("/notices")
+    @Operation(summary = "관리자 담당 업체 건의사항 모두 조회")
+    public ResponseEntity<List<NoticesView>> getAllNotices() {
+
+        List<FindNoticeResult> results = adminReadUseCase.getAllNotices();
+
+        return ResponseEntity.ok(
+                results.stream()
+                        .map(NoticesView::new)
+                        .toList()
+        );
+    }
+
+    @GetMapping("/events")
+    @Operation(summary = "관리자 담당 업체 이벤트 모두 조회")
+    public ResponseEntity<List<EventView>> getAllEvents() {
+        return ResponseEntity.ok(
+                adminReadUseCase
+                        .getAllEvents()
+                        .stream()
+                        .map(EventView::new).toList()
+        );
+    }
+
+    @GetMapping("/suggestions")
+    @Operation(summary = "관리자 담당 업체 건의사항 모두 조회")
+    public ResponseEntity<List<SuggestionCompactView>> getAllSuggestions() {
+
+        List<FindSuggestionResult> result = adminReadUseCase.getAllSuggestions();
+
+        return ResponseEntity.ok(
+                result.stream()
+                        .map(SuggestionCompactView::new)
+                        .toList()
+        );
+    }
+
+    @GetMapping("/additional-services")
+    @Operation(summary = "관리자 담당 업체 부가 서비드 모두 조회")
+    public ResponseEntity<List<AdditionalServiceView>> getAllAdditionalServices() {
+        return ResponseEntity.ok(
+                adminReadUseCase
+                        .getAllAdditionalServices()
+                        .stream()
+                        .map(AdditionalServiceView::new).toList()
+        );
+    }
+
+    @GetMapping("/facilities")
+    @Operation(summary = "관리자 담당 업체 부대시설 모두 조회")
+    public ResponseEntity<List<FacilityView>> getAllFacilities() {
+        return ResponseEntity.ok(
+                adminReadUseCase
+                        .getAllFacilities()
+                        .stream()
+                        .map(FacilityView::new).toList()
+        );
+    }
+
+    @GetMapping("/parks")
+    @Operation(summary = "관리자 담당 업체 테마파크 모두 조회")
+    public ResponseEntity<List<ThemeParkView>> getAllThemeParks() {
+        return ResponseEntity.ok(
+                adminReadUseCase
+                        .getAllThemeParks()
+                        .stream()
+                        .map(ThemeParkView::new).toList()
+        );
     }
 }
