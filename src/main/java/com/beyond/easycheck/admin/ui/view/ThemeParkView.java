5fd1702f9292189b808145b871b5c36fcc159d66
yongun2 <djkye0001@naver.com>
2024-10-22 20:07:40 +0900

    feat: 해당 업체 관리자와 관련된 테마파크 전체 조회 기능

---
+++ src/main/java/com/beyond/easycheck/admin/ui/view/ThemeParkView.java
@@ -0,0 +1,29 @@
+package com.beyond.easycheck.admin.ui.view;
+
+import com.beyond.easycheck.admin.application.service.AdminReadUseCase;
+import com.beyond.easycheck.admin.application.service.AdminReadUseCase.FindThemeParkResult;
+import com.beyond.easycheck.themeParks.application.service.ThemeParkReadUseCase;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.util.List;
+
+@Getter
+@ToString
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ThemeParkView {
+    private final Long id;
+    private final String name;
+    private final String description;
+    private final String location;
+    private final List<String> imageUrls;
+
+    public ThemeParkView(FindThemeParkResult themePark) {
+        this.id = themePark.id();
+        this.name = themePark.name();
+        this.description = themePark.description();
+        this.location = themePark.location();
+        this.imageUrls = themePark.images();
+    }
+}
--- src/main/java/com/beyond/easycheck/themeParks/infrastructure/repository/ThemeParkRepository.java
+++ src/main/java/com/beyond/easycheck/themeParks/infrastructure/repository/ThemeParkRepository.java
@@ -1,11 +1,15 @@
 package com.beyond.easycheck.themeParks.infrastructure.repository;
 
 import com.beyond.easycheck.themeParks.infrastructure.entity.ThemeParkEntity;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface ThemeParkRepository extends JpaRepository<ThemeParkEntity, Long> {
+
+    @EntityGraph(attributePaths = {"accommodation"})
+    List<ThemeParkEntity> findAllByAccommodation_Id(Long accommodationId);
 
     boolean existsByNameAndLocation(String name, String location);
 
