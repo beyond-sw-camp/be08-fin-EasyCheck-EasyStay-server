64ff7c6ed884a4b33f55acc009da334e97c8f2f5
yongun2 <djkye0001@naver.com>
2024-10-24 19:58:48 +0900

    feat: 이메일 형식 검증 어노테이션 추가

--- src/main/java/com/beyond/easycheck/user/ui/requestbody/UserLoginRequest.java
+++ src/main/java/com/beyond/easycheck/user/ui/requestbody/UserLoginRequest.java
@@ -1,9 +1,11 @@
 package com.beyond.easycheck.user.ui.requestbody;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 
 public record UserLoginRequest(
         @NotEmpty
+        @Email
         String email,
         @NotEmpty
         String password
